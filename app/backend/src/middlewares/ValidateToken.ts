0321dffeadde8605f13496a2182e9a26bc364be3
Guilherme <guilhermeevencioluz@gmail.com>
2022-10-31 18:29:44 -0300

    feat: add ValidateToken middleware to the project.

---
+++ app/backend/src/middlewares/ValidateToken.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import CustomError from '../Error/CustomError';
+import TokenService from '../services/TokenService';
+
+export default async function ValidateToken(req: Request, res: Response, next: NextFunction) {
+  try {
+    const token = req.headers.authorization;
+    if (!token) throw new CustomError('Token must be a valid token', 401);
+    const tokenInfo = TokenService.validateToken(token);
+
+    if (!tokenInfo.email) {
+      throw new CustomError('Token must be a valid token', 401);
+    }
+
+    req.body.tokenEmail = tokenInfo.email;
+    next();
+  } catch (error) {
+    const customError = error as CustomError;
+    next(customError);
+  }
+}
