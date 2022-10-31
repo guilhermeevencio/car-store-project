99ce2de24e04707326d4487e71b6f37ff420db8d
Guilherme <guilhermeevencioluz@gmail.com>
2022-10-31 18:28:59 -0300

    feat: adds TokenService.ts file to the project.

---
+++ app/backend/src/services/TokenService.ts
@@ -0,0 +1,25 @@
+import * as jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import CustomError from '../Error/CustomError';
+import { LoginRequest } from '../interfaces/UserInterfaces';
+
+class TokenService {
+  static createToken(userInfo: LoginRequest): string {
+    const secret = process.env.JWT_SECRET || 'jwt_secret';
+    const dataStoredInToken: LoginRequest = userInfo;
+
+    return jwt.sign(dataStoredInToken, secret);
+  }
+
+  static validateToken(token: string): jwt.JwtPayload {
+    try {
+      const secret = process.env.JWT_SECRET || 'jwt_secret';
+      const data = jwt.verify(token, secret) as jwt.JwtPayload;
+      return data;
+    } catch (error) {
+      throw new CustomError('Token must be a valid token', 401);
+    }
+  }
+}
+
+export default TokenService;
