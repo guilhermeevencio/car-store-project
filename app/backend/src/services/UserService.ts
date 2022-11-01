471d1c28aebccf18229d5f5f25ea73789e3932f5
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-01 10:18:28 -0300

    feat: Implements UserService class.

---
+++ app/backend/src/services/UserService.ts
@@ -0,0 +1,58 @@
+import CustomError from "../Error/CustomError";
+import UserModel from "../database/models/UserModel";
+import {
+  IUserCredentials,
+  IUserLoginResponse,
+} from "../interfaces/UserInterfaces";
+import TokenService from "../services/TokenService";
+import HandleWithPassword from "../services/PasswordService";
+
+export default class UserService {
+  private userCredentials: IUserCredentials;
+
+  static async findByEmail(email: string): Promise<UserModel | null> {
+    const user = await UserModel.findOne({ where: { email } });
+    return user;
+  }
+
+  async execute(userCredentials: IUserCredentials): Promise<IUserLoginResponse> {
+    this.userCredentials = userCredentials;
+    const { email, password } = this.userCredentials;
+    const user = await UserService.findByEmail(email);
+
+    if (!user) {
+      throw new CustomError("Incorrect email or password", 401);
+    }
+
+    const isPasswordCorrect = await HandleWithPassword.comparePasswords(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      throw new CustomError("Incorrect email or password", 401);
+    }
+
+    const token = TokenService.createToken({
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      password: user.password,
+    });
+
+    return {
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token,
+    };
+  }
+
+  async validate(tokenEmail: string): Promise<UserModel> {
+    const user = await UserService.findByEmail(tokenEmail);
+
+    if (!user) throw new CustomError("Invalid Token", 401);
+
+    return user;
+  }
+}
