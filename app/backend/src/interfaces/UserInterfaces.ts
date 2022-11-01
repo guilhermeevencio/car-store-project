26aff57f6fe189d84ee3eec47f687f8ee1bb2e99
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-01 10:17:51 -0300

    feat: Add User Interfaces to the project.

--- app/backend/src/interfaces/UserInterfaces.ts
+++ app/backend/src/interfaces/UserInterfaces.ts
@@ -1,8 +1,32 @@
-interface LoginRequest {
+import UserModel from '../database/models/UserModel'
+
+interface ILoginRequest {
   password: string
   email: string
 }
 
+interface IUserCredentials extends ILoginRequest {
+  role: string
+  username: string
+}
+
+interface IUserLoginResponse {
+  username: string
+  email: string
+  token: string
+  role: string
+}
+
+interface IUserService {
+  execute(userInfo: ILoginRequest): Promise<IUserLoginResponse>
+  validate(email: string): Promise<UserModel>
+}
+
+
+
 export {
-  LoginRequest,
+  ILoginRequest,
+  IUserCredentials,
+  IUserService,
+  IUserLoginResponse,
 }
