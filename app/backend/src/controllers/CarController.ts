be8eea55f822140550eb2f3e41fc068ed93d1384
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-02 20:25:36 -0300

    feat: implements car register and update controllers and routes.

--- app/backend/src/controllers/CarController.ts
+++ app/backend/src/controllers/CarController.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { ICarService } from '../interfaces/CarInterfaces';
+import { ICarRequestInfo, ICarResponse, ICarService } from '../interfaces/CarInterfaces';
 import CustomError from '../Error/CustomError';
 
 export default class CarController {
@@ -23,6 +23,33 @@ export default class CarController {
 
       res.status(200).json(carResponse);
       
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async registerCar(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const carDataInfo: ICarRequestInfo = req.body;
+      if (!carDataInfo) throw new CustomError('Some required fields are missing', 400 );
+
+      const registeredCar = await this.carService.registerCar(carDataInfo);
+
+      res.status(201).json(registeredCar);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async updateCar(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const carUpdateInfoFromRequest:ICarResponse = req.body;
+      
+      if (!carUpdateInfoFromRequest) throw new CustomError('Some required fields are missing', 400 );
+      
+      await this.carService.updateCar(carUpdateInfoFromRequest);
+
+      res.status(200).json({ message: 'Successfully updated!' })
     } catch (error) {
       next(error);
     }
--- app/backend/src/routes/cars.routes.ts
+++ app/backend/src/routes/cars.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import CarController from '../controllers/CarController';
 import CarService from '../services/CarService';
 import { ICarService } from '../interfaces/CarInterfaces';
+import ValidateToken from '../middlewares/ValidateToken';
 
 const carService: ICarService = new CarService();
 const carController = new CarController(carService)
@@ -15,6 +16,14 @@ CarsRoutes.get('/cars', (req, res, next) => {
 
 CarsRoutes.get('/cars/:id', (req, res, next) => {
   carController.getCarById(req, res, next);
+});
+
+CarsRoutes.post('/cars', ValidateToken, (req, res, next) => {
+  carController.registerCar(req, res, next);
+});
+
+CarsRoutes.put('/cars', ValidateToken, (req, res, next) => {
+  carController.updateCar(req, res, next);
 });
 
 export default CarsRoutes
