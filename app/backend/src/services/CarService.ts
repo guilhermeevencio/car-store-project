7fbeb5ea35958b41134191730d46b32e7b3a9c65
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-02 20:23:36 -0300

    feat: implements registerCar method in CarService class.

--- app/backend/src/services/CarService.ts
+++ app/backend/src/services/CarService.ts
@@ -1,11 +1,12 @@
 import CarModel from '../database/models/CarModel';
 import CustomError from '../Error/CustomError';
-import { ICarResponse, ICarService } from '../interfaces/CarInterfaces';
+import { ICarRequestInfo, ICarResponse, ICarService } from '../interfaces/CarInterfaces';
 
 export default class CarService implements ICarService {
+  constructor(private carModel = CarModel) {};
 
   async getAllCars(): Promise<ICarResponse[]> {
-    const cars = await CarModel.findAll()
+    const cars = await this.carModel.findAll()
 
     const carDataInfo: ICarResponse[] =  cars.map((car) => {
       const {id, name, brand, model, imageUrl, price} = car
@@ -17,7 +18,14 @@ export default class CarService implements ICarService {
   }
 
   async getCarById(paramId: number): Promise<ICarResponse> {
-    const car = await CarModel.findOne({ where: { id: paramId } });
+    const car = await this.carModel.findOne({ where: { id: paramId } });
+    const {id, name, brand, model, imageUrl, price} = car as CarModel
+
+    return {id, name, brand, model, imageUrl, price};
+  }
+
+  async registerCar(carRequestInfo: ICarRequestInfo): Promise<ICarResponse> {
+    const car = await this.carModel.create({...carRequestInfo});
     const {id, name, brand, model, imageUrl, price} = car as CarModel
 
     return {id, name, brand, model, imageUrl, price};
