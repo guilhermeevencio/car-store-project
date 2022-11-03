78e59e8e76def99e1c7e3b83356a0f541e9d7f71
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-03 14:46:46 -0300

    feat: create a PageNotImplement page.

---
+++ app/frontend/src/pages/PageNotImplemented/index.tsx
@@ -0,0 +1,9 @@
+export function PageNotImplemented() {
+  return (
+    <div className="flex justify-center items-center font-black text-5xl w-full h-screen m-auto text-center bg-slate-800 text-white">
+      <h1 className="bg-red-900 p-12 rounded-3xl">
+        🚧 Página em Desenvolvimento! 🚧
+      </h1>
+    </div>
+  )
+}
