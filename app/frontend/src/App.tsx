0b2ccc3218d0b0a5c8b17920046f2ff83ee73964
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-03 10:28:49 -0300

    feat: implements BrowserRouter component.

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
 }
 
 export default App
