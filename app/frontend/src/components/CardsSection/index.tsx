10ce699dddba67b1f9bd06676ce67b333d32de73
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-04 16:13:14 -0300

    feat: get cars from database and show on screen.

--- app/frontend/src/components/CardsSection/index.tsx
+++ app/frontend/src/components/CardsSection/index.tsx
@@ -1,12 +1,24 @@
+import { useContext, useEffect } from 'react'
 import { ItemCards } from '../../components/ItemCard'
-import { carListMock } from '../../mock/carListMock'
+import AppContext, { IAppContext } from '../../context/AppContext'
+import { carsRequest } from '../../services/requests'
 
 export function CardsSection() {
+  const { carList, setCarList } = useContext(AppContext) as IAppContext
+  useEffect(() => {
+    const fetchCars = async () => {
+      const cars = await carsRequest()
+
+      setCarList(cars)
+    }
+    fetchCars()
+  }, [])
+
   return (
-    <div className="flex flex-row flex-wrap justify-center items-center">
-      {carListMock.map((car) => (
-        <ItemCards key={car.id} car={car} />
-      ))}
+    <div className="flex flex-row flex-wrap justify-center items-center mt-16">
+      {carList
+        ? carList.map((car) => <ItemCards key={car.id} car={car} />)
+        : null}
     </div>
   )
 }
