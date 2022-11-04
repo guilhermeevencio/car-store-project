4ffaa73a24caec4682ab8b6d75ebb6d9dea8d552
Guilherme <guilhermeevencioluz@gmail.com>
2022-11-04 16:11:58 -0300

    feat: create frontend app routes.

--- app/frontend/src/Router.tsx
+++ app/frontend/src/Router.tsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
+import AdminLayout from './layouts/AdminLayout'
 import DefaultLayout from './layouts/DefaultLayout'
 import { Admin } from './pages/Admin'
+import { AdminCreate } from './pages/AdminCreate'
+import { AdminRemove } from './pages/AdminRemove'
+import { AdminUpdate } from './pages/AdminUpdate'
 import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 import { PageNotImplemented } from './pages/PageNotImplemented'
@@ -11,9 +15,15 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
       </Route>
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/" element={<AdminLayout />}>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/create" element={<AdminCreate />} />
+        <Route path="/admin/update" element={<AdminUpdate />} />
+        <Route path="/admin/remove" element={<AdminRemove />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/cars/:id" element={<PageNotImplemented />} />
+      <Route path="/not-implemented" element={<PageNotImplemented />} />
     </Routes>
   )
 }
